b430f179af30c875f8de8b15be80dd16b61bfd65
Afonso Neves <up202108884@up.pt>
2024-11-02 20:29:26 +0000

    adding missing elements

--- TP1.hs
+++ TP1.hs
@@ -2,10 +2,6 @@ import qualified Data.List
 import qualified Data.Array
 import qualified Data.Bits
 
--- PFL 2024/2025 Practical assignment 1
-
--- Uncomment the some/all of the first three lines to import the modules, do not change the code of these lines.
-
 type City = String
 type Path = [City]
 type Distance = Int
@@ -405,3 +401,23 @@ travelSales roadmap
             n = length (cities roadmap)
             matrix = toAdjMatrix roadmap
             memo = solveTSP matrix (initializeMemoTable matrix start n) start n 3
+
+------------------------------------------------------------------------------------------------
+-- Function 10 | Returns a solution to the Traveling Salesman Problem (TSP) using Brute Force.
+------------------------------------------------------------------------------------------------
+
+tspBruteForce :: RoadMap -> Path
+tspBruteForce = undefined
+
+---------------
+-- TEST CASES
+---------------
+
+gTest1 :: RoadMap
+gTest1 = [("7","6",1),("8","2",2),("6","5",2),("0","1",4),("2","5",4),("8","6",6),("2","3",7),("7","8",7),("0","7",8),("1","2",8),("3","4",9),("5","4",10),("1","7",11),("3","5",14)]
+
+gTest2 :: RoadMap
+gTest2 = [("0","1",10),("0","2",15),("0","3",20),("1","2",35),("1","3",25),("2","3",30)]
+
+gTest3 :: RoadMap
+gTest3 = [("0","1",4),("2","3",2)]
